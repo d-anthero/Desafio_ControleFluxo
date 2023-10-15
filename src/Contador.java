a232e0a0ae282fa45f4088ade94cedbe9b25d64a
d-anthero <d.anthero09@gmail.com>
2023-10-15 13:03:57 -0300

    update desafio_controlefluxo

---
+++ src/Contador.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Contador {
+	public static void main(String[] args) {
+		Scanner terminal = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro");
+		int parametroUm = terminal.nextInt();
+		System.out.println("Digite o segundo parâmetro");
+		int parametroDois = terminal.nextInt();
+		
+		try { contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException err) {
+			System.out.println(err);
+		}
+		
+	}
+	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+		if(parametroDois > parametroUm){    
+            int contagem = parametroDois - parametroUm;
+                for(int i = 0; i < contagem; i++){System.out.println("Imprimindo número " + (i + 1));}
+        }
+        else{throw new ParametrosInvalidosException("Segundo parâmetro deve ser maior que o primeiro");}
+	}
+}
